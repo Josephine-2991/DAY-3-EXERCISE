e31c2c3010be25b88d5610cd1f449c47bc289bf4
Josephine-2991 <111166797+Josephine-2991@users.noreply.github.com>
2022-10-22 22:44:05 +0100

    added files

---
+++ DAY3.js
@@ -0,0 +1,93 @@
+//NUMBER 1
+let firstName= "Josephine"
+let lastName = "Ohwifo"
+let country= "Nigeria"
+let city="Lagos"
+let age= 30
+let isMarried = "Single"
+let year= 2022
+
+console.log(typeof (firstName))
+console.log(typeof (lastName))
+console.log(typeof (country))
+console.log(typeof (city))
+console.log(typeof (age))
+console.log(typeof (isMarried))
+console.log(typeof (year))
+
+//NUMBER 2
+console.log(typeof "10"==10)
+
+//NUMBER 3
+console.log(typeof parseInt("9.8"==10))
+
+//NUMBER 4
+//Truthy values
+let num=3
+console.log(num)
+let nameOf = "Joe"
+console.log(nameOf)
+let item = bag
+console.log(item)
+
+//falsy values
+let house=null
+console.log(Boolean(house))
+let hope=""
+console.log(Boolean(hope))
+let goat
+console.log(boolean(goat))
+
+//NUMBER 5
+4 > 3  //true
+4 >= 3 //true
+4 < 3  //false
+4 <= 3 //false
+4 == 4  //true
+4 === 4  //true
+4 != 4   //false
+4 !== 4   //false
+4 != '4'   //false
+4 == '4'   //true
+4 === '4'  //false
+
+console.log(4 > 3) 
+console.log(4 >= 3)
+console.log (4 < 3)
+console.log(4 <= 3)
+console.log(4 == 4)
+console.log(4 === 4)
+console.log(4 != 4)
+console.log(4 !== 4)
+console.log(4 != '4')
+console.log(4 == '4')
+console.log(4 === '4')
+
+let python=python
+let jargon= jargon
+console.log(python.length !== jargon.length)
+
+//NUMBER 6
+4 > 3 && 10 < 12  // true
+4 > 3 && 10 > 12  // false
+4 > 3 || 10 < 12   //true
+4 > 3 || 10 > 12   //true
+!(4 > 3) // false
+!(4 < 3) // true
+!(false) // true
+!(4 > 3 && 10 < 12) // false
+!(4 > 3 && 10 > 12) // false
+!(4 === '4') // true
+
+console.log(4 > 3 && 10 < 12)
+console.log (4 > 3 && 10 > 12)
+console.log (4 > 3 || 10 < 12)
+console.log (4 > 3 || 10 > 12)
+console.log (!(4 > 3))
+console.log (!(4 < 3))
+console.log (!(false))
+console.log (!(4 > 3 && 10 < 12))
+console.log (!(4 > 3 && 10 > 12))
+console.log(!(4 === '4'))
+
+//Number 7
